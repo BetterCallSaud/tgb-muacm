d81a6ed5d3a4a1666c1e717541fe18c372d8aba3
SuperbSaud <48680823+SuperbSaud@users.noreply.github.com>
2021-06-12 20:35:46 +0530

    Added trivia code

--- bot.py
+++ bot.py
@@ -12,209 +12,15 @@ client = discord.Client()
 # TOKEN removed for security purposes
 TOKEN = ''
 
+trivia_api = [
+              "https://opentdb.com/api.php?amount=1&category=15" # Video Games
+              "https://opentdb.com/api.php?amount=1&category=18", # Science Computer
+              "https://opentdb.com/api.php?amount=1&category=30" # Science & Gadgets
+             ]
+
 user_input = ["tgb hello", "tgb hii", "tgb hi", "tgb bro", "tgb yo", "tgb yoohoo", "tgb hey", "tgb ola", "tgb wassup", "tgb dude", "tgb what up dude", "tgb ciao", "tgb buddy"]
 
 bot_reply = ["Hello", "Heya", "Ciao", "Hey", "Heya, how's it going", "Hey, wassup", "Good to see you", "Great to see you", "Glad to see you", "Look who it is! It's a bird... it's a plane... it's ", "Nice to see you again", "Hi there","Long time no see","Howdy", "Hiya", "Yoohoo", "Ola", "Bonjour","What's going on","Look who it is. It's"]
-
-trivia = [
-    "The Firefox logo isn’t a fox.",
-    "The first Apple logo isn’t what you would think.",
-    "Google rents out goats",
-    "The name for “robot” has dark origins",
-    "The first-ever VCR was the size of a piano",
-    "Samsung is 38 years and 1 month older than Apple",
-    "A Petabyte is a lot of data",
-    "Domain name registration used to be free",
-    "Megabytes used to weigh hundreds of pounds",
-    "The Radio took 38 years to reach an audience of 50 million",
-    "The first camera needed an incredibly long exposure",
-    "Credit card chips have been around for a LONG time",
-    'Google rents out goats',
-    'The name for “robot” has dark origins',
-    'The first-ever VCR was the size of a piano',
-    'Samsung is 38 years and 1 month older than Apple',
-    'A Petabyte is a lot of data',
-    'Domain name registration used to be free',
-    'Megabytes used to weigh hundreds of pounds',
-    'The Radio took 38 years to reach an audience of 50 million',
-    'The first camera needed an incredibly long exposure',
-    'Credit card chips have been around for a LONG time',
-    'Alexa is always listening to your conversations',
-    'People read faster or slower depending on what they read from',
-    'GPS is free for some',
-    'There are Amish computers',
-    'Mac computers were named after the apple',
-    'The first computer mouse wasn’t made from plastic',
-    'Which came first, Spam mail or Spam meat',
-    'The original Xbox had sound snippets of real space missions',
-    'The majority of the people plug in their USB wrong',
-    'Steve Jobs used sleight of hand at the first iPhone presentation',
-    'The first alarm clock could only ring at one time',
-    'Computer Security Day is celebrated on November 30th',
-    'The government used PlayStation 3’s… but not for gaming',
-    'The first online gaming was before the year 2000',
-    'The first product scanned was a packet of chewing gum in 1974',
-    'You’re in good hands if your surgeon was a gamer',
-    'iTunes has unusual Terms & Conditions',
-    'Nintendo didn’t start as a video games company',
-    'Apollo 11 astronauts couldn’t afford insurance',
-    'People are still using dial-up',
-    "You can spell your email in Morse code",
-    'Yahoo’s original name was a mouthful',
-    'Everyone uses Google as a spellchecker',
-    'The first word to ever be auto-corrected was “teh"',
-    'The Nintendo Game Boy went to space',
-    'PlayStation 1 had Scratch and Sniff discs',
-    '“Android” is gender-specific',
-    'Google searches hit the billions every month',
-    "There’s a name for when you feel your phone vibrate… but it doesn’t",
-    'Smoking will void your Apple warranty',
-    'Technology is now influencing baby names',
-    'Blind people can use cell phones',
-    'Google’s first tweet was gibberish',
-    'The first cell phone call was in New York City',
-    'The first commercial text message was sent in 1992',
-    'Over 6,000 new computer viruses are created and released every month',
-    'There are more likes than photos on Facebook',
-    'iPhones were almost in the shape of an apple',
-    'Comic Sans is the most hated font in the world',
-    'NASA’s internet speed is 91 GB per second',
-    'Nokia is the largest company from Finland',
-    'More people have cell phones than toilets',
-    'The Apple Lisa was the first commercial computer with a Graphical User Interface (GUI) and a mouse',
-    'Some people are afraid of technology',
-    'The most expensive phone number cost millions',
-    'Mark Zuckerberg is color blind',
-    '40% of American couples meet online',
-    'Music content makes up 5% of YouTube',
-    'Finding a security bug in Facebook’s code will pay off',
-    'Kids that are on social media for 1 hour a day have less chance of being happy',
-    'MySpace lost all of its content before 2016',
-    'Nearly one third of divorces are because of Facebook',
-    'Using a thinner font can save printer ink',
-    'The QWERTY keyboard was originally designed to slow you down',
-    'The first webpage is still running',
-    'Some countries skipped the era of landlines,'
-    'The passwords for the nuclear missiles were just a string of zero’s',
-    'Over 90% of the world’s currency is digital',
-    'Millions of hours of TV and movies are watched every day on Netflix',
-    'Technical degrees are almost useless by the time you graduate',
-    'There’s a term for old people who use the internet',
-    'Tech companies often test their products in New Zealand',
-    'There are fake Apple stores in China',
-    'Until 2010, carrier pigeons were faster than the internet',
-    'The first photo ever uploaded to the internet was a comedy band',
-    'Every advertisement for iPhone’s have 9:41 set as the time',
-    'A “jiffy” is a real measurement',
-    'An average 21 year old has spent 5,000 hours playing video games',
-    'Most of today’s successful companies started in garages',
-    'Most internet traffic isn’t from real humans',
-    'CAPTCHA is a long acronym',
-    'The three most common passwords are also the weakest',
-    'There wasn’t an app store in the first iPhone',
-    'We only keep 1 out of every 10 apps we try',
-    'Digital music sales surpassed physical sales in 2014',
-    'The @ symbol was chosen kind of randomly',
-    'There is a machine that can predict heart attacks',
-    'There is also artificial intelligence than can predict epidemics',
-    'The Amazon’s robot workers skyrocketed in less than five years',
-    'Digital tech is good for the environment',
-    'The average Facebook user has less than 200 friends',
-    'Google uses the same amount of energy as 200,000 homes',
-    'The first computer virus was harmless',
-    'There are only 21 million Bitcoins that can be mined in total',
-    'Filipinos use social media more than Americans',
-    'Most of the purchases in China are done with mobile phones',
-    'Robot laws are being put into place',
-    'Millions of tons of technology are thrown out each year',
-    'People read faster or slower depending on what they read from',
-    'GPS is free… for some',
-    'There are Amish computers',
-    'Mac computers were named after the apple',
-    'The first computer mouse wasn’t made from plastic',
-    'Which came first, Spam mail or Spam meat?',
-    'The original Xbox had sound snippets of real space missions',
-    'The majority of the people plug in their USB wrong',
-    'Steve Jobs used sleight of hand at the first iPhone presentation',
-    'The first alarm clock could only ring at one time',
-    'Computer Security Day is celebrated on November 30th',
-    'The government used PlayStation 3’s… but not for gaming',
-    'The first online gaming was before the year 2000',
-    'The first product scanned was a packet of chewing gum in 1974',
-    'You’re in good hands if your surgeon was a gamer',
-    'iTunes has unusual Terms & Conditions',
-    'Nintendo didn’t start as a video games company',
-    'Apollo 11 astronauts couldn’t afford insurance',
-    'People are still using dial-up',
-    'You can spell your email in Morse code',
-    'Yahoo’s original name was a mouthful',
-    'Everyone uses Google as a spellchecker',
-    'The first word to ever be auto-corrected was “teh”',
-    'The Nintendo Game Boy went to space',
-    'PlayStation 1 had Scratch and Sniff discs',
-    '“Android” is gender-specific',
-    'Google searches hit the billions every month',
-    "There’s a name for when you feel your phone vibrate… but it doesn’t",
-    'Smoking will void your Apple warranty',
-    'Technology is now influencing baby names',
-    'Blind people can use cell phones',
-    'Google’s first tweet was gibberish',
-    'The first cell phone call was in New York City',
-    'The first commercial text message was sent in 1992',
-    'Over 6,000 new computer viruses are created and released every month',
-    'There are more likes than photos on Facebook',
-    'iPhones were almost in the shape of an apple',
-    'Comic Sans is the most hated font in the world',
-    'NASA’s internet speed is 91 GB per second',
-    'Nokia is the largest company from Finland',
-    'More people have cell phones than toilets',
-    'The Apple Lisa was the first commercial computer with a Graphical User Interface (GUI) and a mouse',
-    'Some people are afraid of technology',
-    'The most expensive phone number cost millions',
-    'Mark Zuckerberg is color blind',
-    '40% of American couples meet online',
-    'Music content makes up 5% of YouTube',
-    'Finding a security bug in Facebook’s code will pay off',
-    'Kids that are on social media for 1 hour a day have less chance of being happy',
-    'MySpace lost all of its content before 2016',
-    'Nearly one third of divorces are because of Facebook',
-    'Using a thinner font can save printer ink',
-    'The QWERTY keyboard was originally designed to slow you down',
-    'The first webpage is still running',
-    'Some countries skipped the era of landlines',
-    "The passwords for the nuclear missiles were just a string of zero’s",
-    'Over 90% of the world’s currency is digital',
-    'Millions of hours of TV and movies are watched every day on Netflix',
-    'Technical degrees are almost useless by the time you graduate',
-    'There’s a term for old people who use the internet',
-    'Tech companies often test their products in New Zealand',
-    'There are fake Apple stores in China',
-    'Until 2010, carrier pigeons were faster than the internet',
-    'The first photo ever uploaded to the internet was a comedy band',
-    'Every advertisement for iPhone’s have 9:41 set as the time',
-    'A “jiffy” is a real measurement',
-    'An average 21 year old has spent 5,000 hours playing video games',
-    'Most of today’s successful companies started in garages',
-    'Most internet traffic isn’t from real humans',
-    'CAPTCHA is a long acronym',
-    'The three most common passwords are also the weakest',
-    'There wasn’t an app store in the first iPhone',
-    'We only keep 1 out of every 10 apps we try',
-    'Digital music sales surpassed physical sales in 2014',
-    'The @ symbol was chosen kind of randomly',
-    'There is a machine that can predict heart attacks',
-    'There is also artificial intelligence than can predict epidemics',
-    'The Amazon’s robot workers skyrocketed in less than five years',
-    'Digital tech is good for the environment',
-    'The average Facebook user has less than 200 friends',
-    'Google uses the same amount of energy as 200,000 homes',
-    'The first computer virus was harmless',
-    'There are only 21 million Bitcoins that can be mined in total',
-    'Filipinos use social media more than Americans',
-    'Most of the purchases in China are done with mobile phones',
-    'Robot laws are being put into place',
-    'Millions of tons of technology are thrown out each year',
-]
 
 
 def quote():
@@ -233,10 +39,6 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
-    msg = message.content.lower()
-    if any(word in msg for word in user_input):
-        await message.channel.send(random.choice(bot_reply) + '  {0.author.name}'.format(message))
 
     if message.content.lower() == 'tgb help':
         embed = discord.Embed(
@@ -285,10 +87,12 @@ async def on_message(message):
         embed.set_image(url='https://i.ibb.co/YWV5Bx0/Mu-ACMlogo.png')
         await message.channel.send(embed=embed)
 
-    if message.content.lower() == 'tgb trivia':
-        embed = discord.Embed(color=discord.Color.random())
-        embed.add_field(name="***Trivia requested***  !", value=trivia[randint(0, len(trivia) - 1)])
-        embed.set_footer(text="\nEnjoy! {0.author.name}".format(message))
+    if message.content.lower() == "tgb trivia":
+        res = requests.get(random.choice(trivia_api))
+        data = res.json()
+        val = data["results"][0]["question"] + "\n**Answer: **" + data["results"][0]["correct_answer"]
+        embed = discord.Embed(colour=discord.Colour.random())
+        embed.add_field(name="**Trivia**", value=val)
         await message.channel.send(embed=embed)
 
     if message.content.lower() == 'tgb meme':
