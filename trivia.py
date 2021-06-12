d9dc89d0148a853e88387eef3febbd757deb510f
SuperbSaud <48680823+SuperbSaud@users.noreply.github.com>
2021-06-12 20:30:25 +0530

    Added trivia code

--- trivia.py
+++ trivia.py
@@ -9,25 +9,14 @@ trivia_api = [
 client = discord.Client()
 
 @client.event
-async def on_ready():
-    print(f"Bot is ready")
-
-
-@client.event
 async def on_message(message):
     if client.user == message.author:
         return
 
-    if message.author.id == 850355314523373569:
-        response = await rs.get_ai_response(message.content)
-        await message.reply(response)
-    await client.process_commands(message)
-
-
-    if message.content.lower() == "trivia":
+    if message.content.lower() == "tgb trivia":
         res = requests.get(random.choice(trivia_api))
         data = res.json()
         val = data["results"][0]["question"] + "\n**Answer :-  **" + data["results"][0]["correct_answer"]
         embed = discord.Embed(colour=discord.Colour.random())
-        embed.add_field(name="***Trivia***  !", value= val)
-        await message.channel.send(embed= embed)
+        embed.add_field(name="**Trivia**", value=val)
+        await message.channel.send(embed=embed)
