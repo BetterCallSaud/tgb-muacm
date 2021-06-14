978cbe6a56497a44bad6547b8347f473eca34541
yashsehgal <yashsehgal.work@gmail.com>
2021-06-14 10:08:09 +0530

    HashRouting added (#7)

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,9 +1,19 @@
 import Home from "./Views/Home/Home.view";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Link } from "react-router-dom";
+import { Switch } from 'react-router';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <HashRouter basename="/">
+        <Switch>
+          <Route 
+            path="/" 
+            exact component={Home} 
+          />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
