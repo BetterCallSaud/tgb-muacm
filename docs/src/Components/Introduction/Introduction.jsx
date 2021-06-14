7a83ce5cb375d162d20d8a7c9f06931abec317d9
yashsehgal <yashsehgal.work@gmail.com>
2021-06-14 09:45:43 +0530

    introduction added - docs created (#7)

---
+++ docs/src/Components/Introduction/Introduction.jsx
@@ -0,0 +1,20 @@
+
+import "./Introduction.css";
+
+function Introduction() {
+  return (
+    <div className="Introduction">
+      <h3 className="ContentHeadline">
+        ➼ Introduction
+      </h3>
+      <div className="content">
+        <p>
+          The bot is especially designed for the MUACM community for performing several commands like greeting to the people who joined the community and responding them. There are several commands like giving the info about the team mates, sending random memes, random quotes, some trivia which you haven't knew and the contact info, like how to reach out the MUACM community.
+          In the bot we used some APIs which sends random memes, quotes and trivia.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Introduction;
